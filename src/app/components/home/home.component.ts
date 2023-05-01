6730c0804b4176829bca14964b91760a463c4b13
lionheartsq <lujogarpin78@gmail.com>
2023-05-01 17:43:15 -0500

    Updated

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UtilsService } from 'src/app/services/utils.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -8,43 +10,105 @@ import { UtilsService } from 'src/app/services/utils.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  arrayDocs: any=[];
-  numeroDocumento: any;
+  tipoJuego:String="puntaje";
+  valorLimite:number=0;
+  nombreUsuario:any;
+
   codigoRespuesta: any;
-  numeroCelular: any;
-  email: any;
-  fechaExpedicion: any;
-  nombre: any;
-  tipoDocumento: any;
 
   constructor(public router: Router, private utilsservice:UtilsService) { }
 
   ngOnInit(): void {
   }
 
-  redireccion(){
-    this.router.navigate(['trivia']);
+  validarNombre(){
+    if(this.nombreUsuario=== undefined || this.nombreUsuario=== ""){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
+
+  validarLimite(){
+    if(this.valorLimite=== undefined || this.valorLimite<1){
+      return false;
+    }
+    else{
+      return true;
+    }
   }
 
   completar(){
-    this.arrayDocs=[];
-    this.utilsservice.getData(this.numeroDocumento).subscribe((data)=>{
-    this.codigoRespuesta=data.code;
-    this.arrayDocs=data.info;
-    this.numeroCelular=this.arrayDocs.celular;
-    this.email=this.arrayDocs.correo;
-    this.fechaExpedicion=this.arrayDocs.fecha_expedicion;
-    this.nombre=this.arrayDocs.nombre;
-    this.tipoDocumento=this.arrayDocs.tipo_doc;
-    }, (err) => {
-      this.arrayDocs=[];
-      this.numeroCelular="";
-      this.email="";
-      this.fechaExpedicion="";
-      this.nombre="";
-      this.tipoDocumento="";
-      //debugger
-    });
+    console.log("tipoJuego:"+this.tipoJuego);
+    console.log("valorLimite:"+this.valorLimite);
+    console.log("nombreUsuario:"+this.nombreUsuario);
+
+    if(this.validarLimite()){
+      if(this.validarNombre()){
+        this.utilsservice.validarUsuario(this.nombreUsuario).subscribe((data)=>{
+          this.codigoRespuesta=data.code;
+          console.log("codigoRespuesta:"+this.codigoRespuesta);
+
+            if(this.codigoRespuesta==="Disponible" || this.codigoRespuesta==="Nuevo"){
+              Swal.fire(
+                {
+                  icon: 'success',
+                  title: 'Sala disponible',
+                  text: 'Sala creada correctamente',
+                  footer: data.info
+                }
+              ).then(() => {
+                // Aquí la alerta se ha cerrado
+                this.router.navigate(['trivia']);
+              });
+            }
+            else{
+              //this.router.navigate(['home']);
+              console.log("codigoRespuestaOcupado:"+this.codigoRespuesta);
+              Swal.fire(
+                {
+                  icon: 'error',
+                  title: 'Usuario no disponible',
+                  html: 'No disponible',
+                  footer: 'No se ha podido utilizar el usuario'
+                }
+              )
+            }
+          }, (err) => {
+              Swal.fire(
+                {
+                  icon: 'error',
+                  title: 'Usuario no disponible',
+                  html: 'El nombre de usuario está en uso, por favor elija uno nuevo',
+                  footer: 'No se ha podido completar el registro'
+                }
+              )
+            //console.log("codigoRespuestaFalla:"+this.codigoRespuesta);
+            //debugger
+          });
+      }else{
+        Swal.fire(
+          {
+            icon: 'error',
+            title: 'Nombre de usuario vacio',
+            html: 'Debe proporcionar un nombre de usuario',
+            footer: 'No se ha podido crear la sala'
+          });
+      }
+    }else{
+      Swal.fire(
+        {
+          icon: 'error',
+          title: 'Valor límite vacio',
+          html: 'Debe proporcionar un valor límite',
+          footer: 'No se ha podido crear la sala'
+        });
+    }
+  }
+
+  testing(){
+
   }
 
 }
--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -11,28 +11,12 @@ import { Router } from '@angular/router';
 })
 export class UtilsService {
 
-  private endpoint="http://certificacion.venko.co:31518/documentomvp/servicio/usuario";
-
-  private endpointFirmantes="http://certificacion.venko.co:31518/documentomvp/servicio/intervinientes";
-
-  private endpointArchivos="http://certificacion.venko.co:31518/documentomvp/servicio/documentos/descargar";
+  private endpointValidarUsuario="http://127.0.0.1:8000/jugador/validar?nombreUsuario=";
 
   constructor(private httpClient: HttpClient) { }
 
-  actualizar(key: any) {
-    return this.httpClient.put(`${this.endpoint}`, key);
-  }
-
-  getData(id:any): Observable<any> {
-    return this.httpClient.get(`${this.endpointFirmantes}/${id}`);
+  validarUsuario(id:any): Observable<any> {
+    return this.httpClient.get(`${this.endpointValidarUsuario}${id}`);
     }
-
-  getFile(id: any) {
-    const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
-    //
-    return this.httpClient.post(`${this.endpointArchivos}/${id}`,{headers: headers},{responseType: 'text'});
-    //    return this.httpClient.post(`${this.endpointArchivos}/${id}`,{headers: headers},{responseType: 'arraybuffer'});
-    //    return this.httpClient.post(`${this.endpointArchivos}/${id}`,{headers: headers},{responseType: 'blob'});
-  }
 
 }
